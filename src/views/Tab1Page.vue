afc253bc76bdc4ca8ab74f5585e3efc2340da909
topcoder520 <15977329365@163.com>
2022-10-02 01:01:04 +0800

    updated

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -9,62 +9,32 @@
       <ion-grid>
         <ion-row class="mouseclick">
           <ion-col size="4">
-            <ion-button @click="leftSingleClick()" @dblclick="leftDoubleClick()" expand="full"
-              style="height:100%;width: 100%;">
-              左键</ion-button>
+            <ion-button @click="MouseScrollMouseUp()" expand="full">上滚</ion-button>
           </ion-col>
           <ion-col size="4">
-            <ion-row>
-              <ion-button @click="MouseScrollMouseUp()" expand="full">上滚</ion-button>
-            </ion-row>
-            <ion-row>
-              <ion-button @click="MouseCenterClick()" expand="full">点击</ion-button>
-            </ion-row>
-            <ion-row>
-              <ion-button @click="MouseScrollMouseDown()" expand="full">下滚</ion-button>
-            </ion-row>
+            <ion-button @click="MouseCenterClick()" expand="full">滚轮</ion-button>
           </ion-col>
           <ion-col size="4">
-            <ion-button @click="MouseRightClick()" style="height:100%" expand="full">右键</ion-button>
+            <ion-button @click="MouseScrollMouseDown()" expand="full">下滚</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="12">
+            <div style="height:300px;width:100%;background:#efefef;margin-top:8px;" @touchstart="moveTouchStart"
+              @touchmove="moveTouchmove" @touchend="moveTouchEnd" @click="leftSingleClick" @dblclick="leftDoubleClick">
+
+            </div>
           </ion-col>
         </ion-row>
         <ion-row class="mousemove">
-          <ion-col>
-            <ion-button @click="MouseMoveLefTop()" expand="full">左上移</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button @click="MouseMoveUp()" expand="full">上移</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button @click="MouseMoveRightTop()" expand="full">右上移</ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row class="mousemove">
-          <ion-col>
-            <ion-button @click="MouseMoveLeft()" expand="full">左移</ion-button>
-          </ion-col>
           <ion-col>
             <ion-button @click="MouseLeftToggle()" expand="full">拖拽</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button @click="MouseMoveRight()" expand="full">右移</ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row class="mousemove">
-          <ion-col>
-            <ion-button @click="MouseMoveLeftDown()" expand="full">左下移</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button @click="MouseMoveDown()" expand="full">下移</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button @click="MouseMoveRightDown()" expand="full">右下移</ion-button>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col>
             <p class="tip">灵敏度：</p>
-            <ion-range :ticks="true" @ionChange="onIonChange" :snaps="true" :min="10" :max="300"></ion-range>
+            <ion-range :ticks="true" @ionChange="onIonChange" :snaps="true" :min="1" :max="100"></ion-range>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -75,12 +45,12 @@
 <script>
 import { defineComponent, getCurrentInstance, ref } from 'vue';
 import { useStore } from 'vuex';
-import { IonRange,IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton } from '@ionic/vue';
+import { IonRange, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton } from '@ionic/vue';
 
 var timer = null;
 export default defineComponent({
   name: 'Tab1Page',
-  components: { IonRange,IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCol, IonGrid, IonRow, IonButton },
+  components: { IonRange, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCol, IonGrid, IonRow, IonButton },
   setup() {
     const store = useStore();
     const { proxy } = getCurrentInstance();
@@ -88,128 +58,150 @@ export default defineComponent({
     const leftToggleStatus = ref(0);
     const move = ref(10);
 
-    const onIonChange = ({ detail })=>{
-      move.value = detail.value;
+    //////////触屏////////////
+    const touchStartPointX = ref(0);
+    const touchStartPointY = ref(0);
+    const longPressFlag = ref(0);
+    const longMoveFlag = ref(0);
+    const moveTouchStart = (event) => {
+      console.log('start', event);
+      touchStartPointX.value = event.targetTouches[0].pageX;
+      touchStartPointY.value = event.targetTouches[0].pageY;
+      //长按事件
+      clearTimeout(longPressFlag.value);
+      clearInterval(longMoveFlag.value);
+      longPressFlag.value = setTimeout(() => {
+        if (moveDistanceX.value == 0 && moveDistanceY.value == 0) {
+          console.log('长按');
+          MouseRightClick();
+        } else {
+          longMoveFlag.value = setInterval(() => {
+            moveFunc(moveDistanceX.value, moveDistanceY.value);
+          }, 200);
+        }
+
+      }, 500);
     };
-    const MouseMoveRightDown = ()=>{
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 45 }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
+
+    const moveDistanceX = ref(0);
+    const moveDistanceY = ref(0);
+    const moveTouchmove = (event) => {
+      // 只监听单指划动，多指划动不作响应
+      if (event.targetTouches.length > 1) {
+        return;
+      }
+      //console.log('move', event);
+      // 实时计算distance
+      moveDistanceX.value = event.targetTouches[0].pageX - touchStartPointX.value;
+      moveDistanceY.value = event.targetTouches[0].pageY - touchStartPointY.value;
     };
-    const MouseMoveDown = ()=> {
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 90}).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
+    const moveTouchEnd = (event) => {
+      console.log('end', event);
+      clearInterval(longMoveFlag.value);
+      clearTimeout(longPressFlag.value);
+      // 只监听单指划动，多指划动不作响应
+      if (event.changedTouches.length == 1) {
+        var distanceX = event.changedTouches[0].pageX - touchStartPointX.value;
+        var distanceY = event.changedTouches[0].pageY - touchStartPointY.value;
+        moveDistanceX.value = 0;
+        moveDistanceY.value = 0;
+        moveFunc(distanceX, distanceY);
+      }
     };
-    const MouseMoveLeftDown = ()=> {
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 135 }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
-    };
-    const MouseMoveRight = ()=> {
-      http.get(store.getters.baseURL+'/mouse/right', { move: move.value }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
-    };
-    const MouseLeftToggle = ()=> {
-      if (leftToggleStatus.value == 0) {
-        leftToggleStatus.value = 1;
-        http.get(store.getters.baseURL+'/mouse/lefttoggle', {}).then((res) => {
-          console.log(res);
-        }).catch((err) => {
-          console.log(err);
-        });
-      } else {
-        leftToggleStatus.value = 0;
-        http.get(store.getters.baseURL+'/mouse/lefttoggleup', {}).then((res) => {
+    //移动
+    const moveFunc = (distanceX, distanceY) => {
+      console.log(distanceX + " " + distanceY);
+      if (distanceX != 0 || distanceY != 0) {
+        if (Math.abs(distanceX) > Math.abs(distanceY)) {
+          if (distanceX > 0) {
+            distanceX = distanceX + move.value;
+          } else {
+            distanceX = -(Math.abs(distanceX) + move.value);
+          }
+        } else {
+          if (distanceY > 0) {
+            distanceY = distanceY + move.value;
+          } else {
+            distanceY = -(Math.abs(distanceY) + move.value);
+          }
+        }
+        http.get(store.getters.baseURL + '/mouse/move2', { distanceX: distanceX, distanceY: distanceY }).then((res) => {
           console.log(res);
         }).catch((err) => {
           console.log(err);
         });
       }
     };
-    const MouseMoveLeft = ()=> {
-      http.get(store.getters.baseURL+'/mouse/left', { move: move.value }).then((res) => {
-        console.log(res);
-        //msg = JSON.stringify(res);
-      }).catch((err) => {
-        console.log(err);
-        //alert(JSON.stringify(err));
-      });
+    /////////////////////
+
+    const onIonChange = ({ detail }) => {
+      move.value = detail.value;
     };
-    const MouseMoveRightTop = ()=> {
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 315 }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
+    const MouseLeftToggle = () => {
+      if (leftToggleStatus.value == 0) {
+        leftToggleStatus.value = 1;
+        http.get(store.getters.baseURL + '/mouse/lefttoggle', {}).then((res) => {
+          console.log(res);
+        }).catch((err) => {
+          console.log(err);
+        });
+      } else {
+        leftToggleStatus.value = 0;
+        http.get(store.getters.baseURL + '/mouse/lefttoggleup', {}).then((res) => {
+          console.log(res);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }
     };
-    const MouseMoveUp = ()=> {
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 270 }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
-    };
-    const MouseMoveLefTop = ()=> {
-      http.get(store.getters.baseURL+'/mouse/move', { move: move.value, angle: 225 }).then((res) => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
-    };
-    const MouseRightClick = ()=> {
-      http.get(store.getters.baseURL+'/mouse/rightclick', {}).then((res) => {
+    const MouseRightClick = () => {
+      http.get(store.getters.baseURL + '/mouse/rightclick', {}).then((res) => {
         console.log(res);
       }).catch((err) => {
         console.log(err);
       });
     };
-    const MouseScrollMouseDown = ()=> {
-      http.get(store.getters.baseURL+'/mouse/wheelscrolldown', { move: move.value }).then((res) => {
+    const MouseScrollMouseDown = () => {
+      http.get(store.getters.baseURL + '/mouse/wheelscrolldown', { move: move.value }).then((res) => {
         console.log(res);
       }).catch((err) => {
         console.log(err);
       });
     };
-    const MouseCenterClick = ()=> {
-      http.get(store.getters.baseURL+'/mouse/centerclick', {}).then((res) => {
+    const MouseCenterClick = () => {
+      http.get(store.getters.baseURL + '/mouse/centerclick', {}).then((res) => {
         console.log(res);
       }).catch((err) => {
         console.log(err);
       });
     };
-    const MouseScrollMouseUp = ()=> {
-      http.get(store.getters.baseURL+'/mouse/wheelscrollup', { move: move.value }).then((res) => {
+    const MouseScrollMouseUp = () => {
+      http.get(store.getters.baseURL + '/mouse/wheelscrollup', { move: move.value }).then((res) => {
         console.log(res);
       }).catch((err) => {
         console.log(err);
       });
     };
-    const leftSingleClick = ()=> {
-      clearTimeout(timer);  //首先清除计时器
+    const leftSingleClick = () => {
+      clearTimeout(timer);  //清除
+      clearTimeout(longPressFlag.value); //清除长按标记
+      clearInterval(longMoveFlag.value);
       timer = setTimeout(() => {
         if (!timer) {
           return;
         }
-        http.get(store.getters.baseURL+'/mouse/leftclick', {}).then((res) => {
+        http.get(store.getters.baseURL + '/mouse/leftclick', {}).then((res) => {
           console.log(res);
         }).catch((err) => {
           console.log(err);
         });
       }, 300);   //大概时间300ms
     };
-    const leftDoubleClick = ()=> {
+    const leftDoubleClick = () => {
       clearTimeout(timer);  //清除
-      http.get(store.getters.baseURL+'/mouse/leftdoubleclick', {}).then((res) => {
+      clearTimeout(longPressFlag.value); //清除长按标记
+      clearInterval(longMoveFlag.value);
+      http.get(store.getters.baseURL + '/mouse/leftdoubleclick', {}).then((res) => {
         console.log(res);
       }).catch((err) => {
         console.log(err);
@@ -218,21 +210,16 @@ export default defineComponent({
 
     return {
       onIonChange,
-      MouseMoveRightDown,
-      MouseMoveDown,
-      MouseMoveLeftDown,
-      MouseMoveRight,
       MouseLeftToggle,
-      MouseMoveLeft,
-      MouseMoveRightTop,
-      MouseMoveUp,
-      MouseMoveLefTop,
       MouseRightClick,
       MouseScrollMouseDown,
       MouseCenterClick,
       MouseScrollMouseUp,
       leftSingleClick,
       leftDoubleClick,
+      moveTouchStart,
+      moveTouchmove,
+      moveTouchEnd,
     }
   },
 });
@@ -253,16 +240,17 @@ ion-button {
 }
 
 ion-range {
-    --bar-background: #bde0fe;
-    --bar-background-active: #3880ff;
-    --bar-height: 8px;
-    --bar-border-radius: 8px;
-    --knob-background: #bde0fe;
-    --knob-size: 60px;
-    --pin-background: #ffafcc;
-    --pin-color: #fff;
-  }
-  .tip{
-    margin: 0px;
-  }
+  --bar-background: #bde0fe;
+  --bar-background-active: #3880ff;
+  --bar-height: 8px;
+  --bar-border-radius: 8px;
+  --knob-background: #bde0fe;
+  --knob-size: 60px;
+  --pin-background: #ffafcc;
+  --pin-color: #fff;
+}
+
+.tip {
+  margin: 0px;
+}
 </style>
