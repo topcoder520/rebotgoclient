358a04715a5105646c41395db66a909e135da80f
topcoder520 <15977329365@163.com>
2022-11-15 20:11:16 +0800

    update

--- src/components/ShortCutContainer.vue
+++ src/components/ShortCutContainer.vue
@@ -6,124 +6,193 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-grid>
-        <ion-row>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('f2')">
-              F2
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('f5')">
-              F5
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('f11')">
-              F11
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('f12')">
-              F12
-            </ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('a,ctrl')">
-              全选
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('x,ctrl')">
-              剪切
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('c,ctrl')">
-              复制
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('v,ctrl')">
-              粘贴
-            </ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('s,ctrl')">
-              保存
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('z,ctrl')">
-              撤销
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('home')">
-              Home
-            </ion-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-button @click="clickdirect('end')">
-              End
-            </ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="6">
-            <ion-button @click="clickdirect('tab')">
-              Tab
-            </ion-button>
-          </ion-col>
-          <ion-col size="6">
-            <ion-button @click="clickdirect('esc')">
-              Esc
-            </ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="12">
-            <ion-button @click="clickdirect('space')">
-              空格
-            </ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col size="6">
-            <ion-button @click="clickdirect('r,cmd')">
-              Ctrl+R
-            </ion-button>
-          </ion-col>
-          <ion-col size="6">
-            <ion-button @click="clickdirect('cmd')">
-              <ion-icon :icon="logoMicrosoft" />
-            </ion-button>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+      <ion-slides :options="slideOpts">
+        <ion-slide>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('f2')">
+                  F2
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('f5')">
+                  F5
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('f11')">
+                  F11
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('f12')">
+                  F12
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('a,ctrl')">
+                  全选
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('x,ctrl')">
+                  剪切
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('c,ctrl')">
+                  复制
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('v,ctrl')">
+                  粘贴
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('s,ctrl')">
+                  保存
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('z,ctrl')">
+                  撤销
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('home')">
+                  Home
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('end')">
+                  End
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="6">
+                <ion-button @click="clickdirect('tab')">
+                  Tab
+                </ion-button>
+              </ion-col>
+              <ion-col size="6">
+                <ion-button @click="clickdirect('esc')">
+                  Esc
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="12">
+                <ion-button @click="clickdirect('space')">
+                  空格
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="6">
+                <ion-button @click="clickdirect('r,cmd')">
+                  Ctrl+R
+                </ion-button>
+              </ion-col>
+              <ion-col size="6">
+                <ion-button @click="clickdirect('cmd')">
+                  <ion-icon :icon="logoMicrosoft" />
+                </ion-button>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-slide>
+        <ion-slide>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('left')">
+                  <ion-icon :icon="arrowBackOutline" />
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('right')">
+                  <ion-icon :icon="arrowForwardOutline" />
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('up')">
+                  <ion-icon :icon="arrowUpOutline" />
+                </ion-button>
+              </ion-col>
+              <ion-col size="3">
+                <ion-button @click="clickdirect('down')">
+                  <ion-icon :icon="arrowDownOutline" />
+                </ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="6">
+                <ion-button @click="KeyInputString" expand="full">Enter(传输)</ion-button>
+              </ion-col>
+              <ion-col size="6">
+                <ion-button @click="clickdirect('backspace')" expand="full">Backspace</ion-button>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="12">
+                <textarea rows="6" ref="textarea" style="width:100%;" v-model="content"></textarea>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-slide>
+      </ion-slides>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { defineComponent, getCurrentInstance } from 'vue';
+import { defineComponent, getCurrentInstance, ref } from 'vue';
 import { useStore } from 'vuex';
 import { arrowBackOutline, arrowForwardOutline, arrowDownOutline, arrowUpOutline, logoMicrosoft } from 'ionicons/icons';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton, IonIcon } from '@ionic/vue';
+import {
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton, IonIcon,
+  IonSlides, IonSlide,
+} from '@ionic/vue';
 import { Clipboard } from '@capacitor/clipboard';
 
 export default defineComponent({
   name: 'ShortCut',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCol, IonGrid, IonRow, IonButton, IonIcon },
+  components: {
+    IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCol, IonGrid, IonRow, IonButton,
+    IonIcon, IonSlides, IonSlide,
+  },
   setup() {
 
     const store = useStore();
     const { proxy } = getCurrentInstance();
     const http = proxy.$api;
+
+    const slideOpts = {
+      initialSlide: 0,
+      speed: 400
+    };
+    setTimeout
+    //传输内容
+    const content = ref('');
+    const KeyInputString = () => {
+      console.log(content.value);
+      const inputstring = content.value;
+      console.log(store.getters.baseURL);
+      http.post(store.getters.baseURL + '/key/Keyinputstring', { content: inputstring }).then((res) => {
+        console.log(res);
+        content.value = '';
+      }).catch((err) => {
+        console.log(err);
+      });
+    };
+
     //方向
     const writeToClipboard = async (text) => {
       await Clipboard.write({
@@ -144,12 +213,16 @@ export default defineComponent({
     };
 
     return {
+      content,
+      KeyInputString,
       arrowBackOutline,
       arrowForwardOutline,
       arrowDownOutline,
       arrowUpOutline,
       logoMicrosoft,
-      clickdirect
+      clickdirect,
+
+      slideOpts,
     };
   }
 });
