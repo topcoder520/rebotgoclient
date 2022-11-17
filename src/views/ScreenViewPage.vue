a4b3be1e1ace7561a21e4c35e272acf24f37068c
topcoder520 <38901719+topcoder520@users.noreply.github.com>
2022-11-17 15:29:37 +0800

    update

--- src/views/ScreenViewPage.vue
+++ src/views/ScreenViewPage.vue
@@ -7,15 +7,15 @@
         </ion-header>
         <ion-content :fullscreen="true">
             <div style="padding:9px;" id="screenbox">
-                <canvas id="screen" ref="screen" style="background-color: #CCCCCC;width: 100%;height: 100%;"></canvas>
+                <canvas id="screen" ref="screen" style="background-color: #CCCCCC;width:100%;height:100%;"></canvas>
             </div>
         </ion-content>
     </ion-page>
 </template>
 <script>
-import { defineComponent, computed, ref, onMounted, } from 'vue';
+import { defineComponent, computed, ref, onActivated, } from 'vue';
 import { useStore } from 'vuex';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, onIonViewDidEnter, } from '@ionic/vue';
 import { useWindowSize } from '@vant/use';
 export default defineComponent({
     name: 'ScreenView',
@@ -26,10 +26,12 @@ export default defineComponent({
         //画布
         const screen = ref(null);
         const context2D = ref(null);
-        onMounted(() => {
+        onIonViewDidEnter(() => {
             context2D.value = screen.value.getContext('2d');
         });
-
+        onActivated(()=>{
+            context2D.value = screen.value.getContext('2d');
+        });
         const getBaseUrl = computed(() => {
             var baseUrl = store.getters.baseURL;
             if (!baseUrl) {
@@ -43,30 +45,31 @@ export default defineComponent({
         const socket = new WebSocket(getBaseUrl.value);
         // Connection opened
         socket.addEventListener('open', function () {
-            socket.send('Hello Server!');
+            socket.send(JSON.stringify({cmd:'text'}));
         });
 
         // Listen for messages
         const imageScreen = ref('');
-        const { width,height } = useWindowSize();
+        const { width, height } = useWindowSize();
         socket.addEventListener('message', function (event) {
+            if(!context2D.value){
+                context2D.value = screen.value.getContext('2d');
+            }
             console.log('Message from server ');
-            console.log('useWindowSize:'+width.value,height.value);
             //console.log(event.data);
             var img = new Image();
             img.src = "data:image/jpeg;base64," + event.data;
-            let dwidth = 300;
-            let dheight = dwidth*3/4;
             img.onload = function () {
-                context2D.value.beginPath();
-                context2D.value.rotate(Math.PI / 2);
-                context2D.value.drawImage(img,0,-300,dheight,dwidth);
-                context2D.value.closePath();
+                console.log('img=>', img.width, img.height);
+                console.log('useWindowSize:' + width.value, height.value);
+                var setRate = 1;
+                console.log('setRate',setRate);
+                context2D.value.drawImage(img, 0,0,300,150); // w h
             }
         });
-        
-        const vheight = ref(height.value-120 + 'px');
-        const vwidth = ref(width.value-30 + 'px');
+
+        const vheight = ref(height.value-200 + 'px');
+        const vwidth = ref(width.value  + 'px');
         return {
             imageScreen,
             screen,
@@ -77,8 +80,8 @@ export default defineComponent({
 });
 </script>
 <style>
-    #screenbox{
-        width: v-bind("vwidth");
-        height: v-bind("vheight");
-    }
+#screenbox {
+    width: v-bind("vwidth");
+    /* height: v-bind("vheight");  */
+}
 </style>
